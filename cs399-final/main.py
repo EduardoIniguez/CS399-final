52e83b61a43bc57798c348de14dfbd39bc109bc7
Jarrin Oishi <jro65@nau.edu>
2015-04-30 13:55:56 -0700

    Carousel Fix
    Slight changes to index page and a fix to the base template to fix a
    javascript error.

--- cs399-final/main.py
+++ cs399-final/main.py
@@ -41,8 +41,13 @@ class Home(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
+
+            template_values = {
+                'user': user,
+            }
+
             template = env.get_template('index.html')
-            self.response.write(template.render())
+            self.response.write(template.render(template_values))
         else:
             self.redirect(users.create_login_url('/'))
 
