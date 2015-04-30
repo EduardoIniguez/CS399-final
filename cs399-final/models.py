77721785397c590e1be433d7599f4b195a8d8b25
ktaed <ktaed@nau.edu>
2015-04-30 15:13:36 -0700

    Corrected Model typos

--- cs399-final/models.py
+++ cs399-final/models.py
@@ -1,18 +1,24 @@
-import webapp2
-import cgi
 from google.appengine.ext import ndb
-from google.appengine.api import mail
-from google.appengine.api import users
 
-from createEval import CreateEvaluation
-from jinja2 import Environment, FileSystemLoader
-env = Environment(loader=FileSystemLoader('templates'))
 
-#model
-class PeerEvalMod(ndb.model):
+class FormMod(ndb.Model):
+    name = ndb.BooleanProperty(default=False)
+    nameReq = ndb.BooleanProperty(default=False)
+    score = ndb.BooleanProperty(default=False)
+    scoreReq = ndb.BooleanProperty(default=False)
+    workAgain = ndb.BooleanProperty(default=False)
+    workAgainReq = ndb.BooleanProperty(default=False)
+    comment = ndb.BooleanProperty(default=False)
+    commentReq = ndb.BooleanProperty(default=False)
+    quest = ndb.StringProperty()
+    questReq = ndb.StringProperty()
+    questType = ndb.StringProperty()
+
+
+class PeerEvalMod(ndb.Model):
     name = ndb.StringProperty()
     nameReq = ndb.BooleanProperty()
-    score = ndb.IntegerProperty(default=100, min=0)
+    score = ndb.IntegerProperty(default=100)
     scoreReq = ndb.BooleanProperty()
     workAgain = ndb.BooleanProperty()
     workAgainReq = ndb.BooleanProperty()
