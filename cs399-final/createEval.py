b35a5a071230e42978ec70a85a3e345c2cb92005
ktaed <ktaed@nau.edu>
2015-04-30 16:28:19 -0700

    createEval now puts to the DB

--- cs399-final/createEval.py
+++ cs399-final/createEval.py
@@ -1,54 +1,59 @@
 import webapp2
 
-import cgi
-from google.appengine.ext import ndb
-from google.appengine.api import mail
-from google.appengine.api import users
-
 from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('templates'))
+
+from models import FormMod
 
 
 class CreateEvaluation(webapp2.RequestHandler):
     def get(self):
         form ="<div class='form-group'><div class='col-sm-offset-2 col-sm-10'>" \
-              "<div class='checkbox' name='nameInclude'><label class='control-label col-sm-2'>" \
-              "<input type='checkbox' checked name='name'>Member Name</label>" \
+              "<div class='checkbox' ><label class='control-label col-sm-2'>" \
+              "<input type='checkbox' checked name='nameInclude'>Member Name</label>" \
               "<label class='control-label col-sm-3'><input type='checkbox' name='nameReq'>" \
               "required?</label></div></div></div><div class='form-group'>" \
-              "<div class='col-sm-offset-2 col-sm-10'><div class='checkbox' name='scoreInclude'><label class='control-label col-sm-2'>" \
-              "<input type='checkbox' checked name='score'>Score </label><label class='control-label col-sm-3'>" \
+              "<div class='col-sm-offset-2 col-sm-10'><div class='checkbox'><label class='control-label col-sm-2'>" \
+              "<input type='checkbox' checked name='scoreInclude'>Score </label><label class='control-label col-sm-3'>" \
               "<input type='checkbox' name='scoreReq'>required?</label></div></div></div>" \
-              "<div class='form-group'><div class='col-sm-offset-2 col-sm-10'><div class='checkbox' name='workAgainInclude'>" \
-              "<label class='control-label col-sm-2'><input type='checkbox' checked name='workAgain'>Work with again </label>" \
+              "<div class='form-group'><div class='col-sm-offset-2 col-sm-10'><div class='checkbox'>" \
+              "<label class='control-label col-sm-2'><input type='checkbox' checked name='workAgainInclude'>Work with again </label>" \
               "<label class='control-label col-sm-3'><input type='checkbox' name='workAgainReq'>required?</label>" \
-              "</div></div></div><div class='form-group' ><div  class='col-sm-offset-2 col-sm-10'><div class='checkbox' name='commentInclude'>" \
-              "<label class='control-label col-sm-2'><input type='checkbox' checked name='comment'>Comment </label>" \
+              "</div></div></div><div class='form-group' ><div  class='col-sm-offset-2 col-sm-10'><div class='checkbox'>" \
+              "<label class='control-label col-sm-2'><input type='checkbox' checked name='commentInclude'>Comment </label>" \
               "<label class='control-label col-sm-3'><input type='checkbox' name='commentReq'>required?</label></div></div></div>" \
-              "<input type='text' hidden='hidden' value='' name='quest'><input hidden='hidden' type='text' value='' name='questReq'><input hidden='hidden' type='text' name='questType'>"
+              "<input type='text' hidden='hidden' name='quest'><input hidden='hidden' type='text' name='questReq'><input hidden='hidden' type='text' name='questType'>"
 
+        # form =
         temp_var = {'form':form}
         template = env.get_template('createEval.html')
         self.response.write(template.render(temp_var))
 
     def post(self):
-            form = ''
-            if self.request.get('name'):
-                form = "pooop"
-            # print self.request.get('name')
-            # if self.request.get(''):
-            #     form+='<div class='form-group'><label class='control-label col-sm-2' for='id_name'>Member Name:</label>' \
-            #           '<div class='col-sm-10'><input type='text' class='form-control' id='id_name'></div></div>'
-            # if self.request.get('score'):
-            #      form += '<div class='form-group'><label class='control-label col-sm-2' for='id_score'>Score:</label>' \
-            #              '<div class='col-sm-10'><input type='number' class='form-control' id='id_score' default=100 ></div></div>'
-            # if self.request.get('workAgain'):
-            #     form += '<div class='form-group'><div class='col-sm-offset-2 col-sm-10'><div class='checkbox'>' \
-            #             '<label class='control-label col-sm-2' for='id_workagain'><input type='checkbox' checked id='id_workagain'>' \
-            #             ' Work with again</label></div></div></div>'
-            temp_var = {'form':form }
-            template = env.get_template('createEval.html')
-            self.response.write(template.render(temp_var))
+        form = ''
+        form = FormMod()
+        if self.request.get("nameInclude"):
+            form.name = True
+            if self.request.get("nameReq"):
+                form.nameReq = True
+        if self.request.get("scoreInclude"):
+            form.score = True
+            if self.request.get("scoreReq"):
+                form.scoreReq = True
+        if self.request.get("workAgainInclude"):
+            form.workAgain = True
+            if self.request.get("workAgainReq"):
+                form.workAgainReq = True
+        if self.request.get("commentInclude"):
+            form.comment = True
+            if self.request.get("commentReq"):
+                form.commentReq = True
+        form.quest = self.request.get('quest')
+        form.questReq = self.request.get('questReq')
+        form.questType = self.request.get('questType')
+        form.put()
+        form = str(form)
+        temp_var = {'form':form }
+        template = env.get_template('createEval.html')
+        self.response.write(template.render(temp_var))
 
-    # def form(self,fields):
-    #     if fields =='':
